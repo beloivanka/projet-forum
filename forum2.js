06085ca149f90095cd8f2d793c3769128dc14c0a
Nadiia Biloivanenko <biloivanka@gmail.com>
2023-10-27 16:09:14 +0200

    modifications inputs js

--- forum2.js
+++ forum2.js
@@ -17,6 +17,7 @@ const myTime = {
 let i = 1; 
 let btnAjouter = document.getElementById("btnAjouter");
 let voirBtn = document.querySelector(".btn-voir");
+let input = document.getElementById("sujetInput");
 let messageSection = document.getElementById("message-field");
 let ajoutSection = document.getElementById("ajout-container");
 let tableauSection = document.getElementById("table-container");
@@ -29,81 +30,92 @@ let auteur = document.getElementById("auteur");
 
 let btnMessage = document.getElementById("btnMessage");
 let inputMessage = document.getElementById("message");
+let errorMessage = document.getElementById("errorMessage");
+let errorMessageAjout = document.getElementById("errorMessageAjout");
 
 
 btnAjouter.addEventListener("click", ajouter);
 
-function ajouter(){  
+function ajouter(e){  
+    if (input.value === ""){
+        e.preventDefault()
+        errorMessageAjout.classList.add("block");
+        input.style.borderColor = "red";
+    }else {
 
-    const date = new Date();
+        const date = new Date();
 
-    let dateCo = date.toLocaleDateString();
-    let timeCo = date.toLocaleTimeString();
+        let dateCo = date.toLocaleDateString();
+        let timeCo = date.toLocaleTimeString();
+        
+        let div1 = document.createElement("tr");
+        div1.id = "div1";
+        document.getElementById("div-parent").appendChild(div1);
     
-    let div1 = document.createElement("tr");
-    div1.id = "div1";
-    document.getElementById("div-parent").appendChild(div1);
-
-    let numberDiv = document.createElement("td");
-    numberDiv.innerHTML = i++;
-    div1.appendChild(numberDiv);
+        let numberDiv = document.createElement("td");
+        numberDiv.innerHTML = i++;
+        div1.appendChild(numberDiv);
+        
+        let sujetDiv = document.createElement("td");
+        sujetDiv.innerHTML = input.value;
+        div1.appendChild(sujetDiv);
+        
+        let dateDiv = document.createElement("td");
+        dateDiv.innerHTML = dateCo + " à " + timeCo;
+        div1.appendChild(dateDiv);
+        
+        let nomDiv = document.createElement("td");
+        nomDiv.innerHTML = objetUser["identifiant"];
+        div1.appendChild(nomDiv);
     
-    let input = document.getElementById("sujetInput").value;
-    let sujetDiv = document.createElement("td");
-    sujetDiv.innerHTML = input;
-    div1.appendChild(sujetDiv);
+        let btnDiv = document.createElement("button");
+        btnDiv.className = "btn-voir";
+        btnDiv.innerHTML = "Voir";
+        div1.appendChild(btnDiv);
     
-    let dateDiv = document.createElement("td");
-    dateDiv.innerHTML = dateCo + " à " + timeCo;
-    div1.appendChild(dateDiv);
+        btnDiv.addEventListener("click", voirClick);
     
-    let nomDiv = document.createElement("td");
-    nomDiv.innerHTML = objetUser["identifiant"];
-    div1.appendChild(nomDiv);
-
-    let btnDiv = document.createElement("button");
-    btnDiv.className = "btn-voir";
-    btnDiv.innerHTML = "Voir";
-    div1.appendChild(btnDiv);
-
-    btnDiv.addEventListener("click", voirClick);
-
-    function voirClick (){
-        messageSection.classList.add("block-message");
-        sujetSection.classList.add("block-stretch");
-        ajoutSection.classList.add("none");
-        tableauSection.classList.add("none");
-
-        let sujetInput2 = input;
-    
-        sujet.innerHTML = sujetInput2;
-        comment.innerHTML = dateCo + " à " + timeCo;
-        auteur.innerHTML = objetUser["identifiant"];
-    
+        function voirClick (){
+            messageSection.classList.add("block-message");
+            sujetSection.classList.add("block-stretch");
+            ajoutSection.classList.add("none");
+            tableauSection.classList.add("none");
+        
+            sujet.innerHTML = input.value;
+            comment.innerHTML = dateCo + " à " + timeCo;
+            auteur.innerHTML = objetUser["identifiant"];
+        
+        }
     }
+
 }
 
 
 btnMessage.addEventListener("click", sendMessage);
 
-function sendMessage () {
-    newMessageSection.classList.add("block-stretch");
+function sendMessage (e) {
+    if (inputMessage.value === ""){
+        e.preventDefault()
+        errorMessage.classList.add("block");
+        inputMessage.style.borderColor = "red";
+    }else{
+        newMessageSection.classList.add("block-stretch");
 
-    const date = new Date();
-
-    let dateCo = date.toLocaleDateString();
-    let timeCo = date.toLocaleTimeString();
-
-    let div2 = document.createElement("tr");
-    div2.id = "div2";
-    document.getElementById("div-parent2").appendChild(div2);
-
-    let input2 = document.getElementById("message").value;
-    let sujet2Div = document.createElement("td");
-    sujet2Div.innerHTML = input2;
-    div2.appendChild(sujet2Div);
-
-    let nom2Div = document.createElement("td");
-    nom2Div.innerHTML = objetUser["identifiant"]+ " le " + dateCo + " à " + timeCo;
-    div2.appendChild(nom2Div);
+        const date = new Date();
+    
+        let dateCo = date.toLocaleDateString();
+        let timeCo = date.toLocaleTimeString();
+    
+        let div2 = document.createElement("tr");
+        div2.id = "div2";
+        document.getElementById("div-parent2").appendChild(div2);
+    
+        let sujet2Div = document.createElement("td");
+        sujet2Div.innerHTML = inputMessage.value;
+        div2.appendChild(sujet2Div);
+    
+        let nom2Div = document.createElement("td");
+        nom2Div.innerHTML = objetUser["identifiant"]+ " le " + dateCo + " à " + timeCo;
+        div2.appendChild(nom2Div);
+    }
 }
